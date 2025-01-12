24eb339254a1f9ffda86320f973b360e462df03e
busine011 <112756675+busine011@users.noreply.github.com>
2025-01-12 19:29:44 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
-var CharsessionId = "abcdefghijklmnopqrstuvwxyz0123456789";
+var CharSessionId = "abcdefghijklmnopqrstuvwxyz0123456789";
 var CharRandomText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 const sleep = async (seconds) => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 const randomText = length => [...Array(length)].map(() => CharRandomText.charAt(Math.floor(Math.random() * 62))).join('');
-const sessionId = () => [...Array(32)].map((_, i) => (i === 8 || i === 12 || i === 16 || i === 20 ? '-' : '') + CharsessionId.charAt(Math.floor(Math.random() * 36))).join('');
+const sessionId = () => [...Array(32)].map((_, i) => (i === 8 || i === 12 || i === 16 || i === 20 ? '-' : '') + CharSessionId.charAt(Math.floor(Math.random() * 36))).join('');
